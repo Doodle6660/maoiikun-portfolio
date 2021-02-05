461a3d36702ea13a07ab685256fcfffd7fac4850
Doodle6660 <ziyadzidane69@gmail.com>
2021-02-05 12:47:50 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <!-- <div id="nav">
+    <!-- <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div> -->
-  <router-view />
+    <router-view />
 </template>
 
-<style></style>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Quattrocento:wght@400;700&family=Quicksand:wght@300;400;500;600;700&family=Sacramento&display=swap");
+</style>
