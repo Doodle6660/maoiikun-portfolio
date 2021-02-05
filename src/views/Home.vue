854c6316bc7ddd3438c0dddc9ab8c39fe67f8996
Doodle6660 <ziyadzidane69@gmail.com>
2021-02-05 20:48:56 +0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,24 @@
 <template>
     <div class="home">
         <app-hello></app-hello>
+        <app-link></app-link>
+        <app-biography></app-biography>
     </div>
 </template>
 
 <script>
 import Hello from "@/components/Hello.vue"
+import QuickLinks from "@/components/QuickLinks.vue"
+import Biography from "@/components/Biography.vue"
 
 export default {
+    data() {
+        return {}
+    },
     components: {
         "app-hello": Hello,
+        "app-link": QuickLinks,
+        "app-biography": Biography,
     },
 }
 </script>
